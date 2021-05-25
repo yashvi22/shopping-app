ec61987890b1c2b03d905ef088b7d7af1be62d2d
yashvi <yashvihp22@gmail.com>
2021-05-25 17:04:16 +0530

    Added stripe API

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {
   selectCartItems,
   selectCartTotal,
@@ -28,10 +29,17 @@ const Checkout = ({ cartItems, cartTotal }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
-    <div className="total">
-      <span>TOTAL: ${cartTotal}</span>
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
+    <div className="total">TOTAL: ${cartTotal}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for Payment
+      <br />
+      4242 4242 4242 4242 - Expiry: any further date <br />
+      CVV: Any three digit
     </div>
+    <StripeCheckoutButton price={cartTotal} />
   </div>
 );
 

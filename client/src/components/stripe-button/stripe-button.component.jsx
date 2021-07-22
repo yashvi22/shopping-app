d3745e8491cae15d68995a2b2ecabf69085d2a05
Yashvi Patel <yashvi.patel@gatewaygroup.com>
2021-07-22 12:56:22 +0530

    final commit

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     "pk_test_51IuxqwSJqeXPtdnXIoj0XPCMCLCJZjvP4Cpus72lSDRasub2GmC7YPxtQfvK0M311DAM3vVApFVjhJtRbeoOn9JS00FDXxD9g4";
 
-const onToken=token=>{
-    console.log(token)
-    alert('Payment successfull')
-}
+  const onToken = token => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert("succesful payment");
+      })
+      .catch(error => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
+  };
 
   return (
     <StripeCheckout
@@ -20,7 +36,7 @@ const onToken=token=>{
       image="https://svgshare.com/i/CUz.svg"
       description={`Your total is ${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
